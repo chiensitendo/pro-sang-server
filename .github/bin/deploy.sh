e25a7c339e2bbbad8e07b47194c5550f72c4096c
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-04 22:55:45 +0700

    try to get the env

--- .github/bin/deploy.sh
+++ .github/bin/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "DEPL_FOLDER_PATH: $DEPL_FOLDER_PATH"
+echo "FOLDER_PATH: $FOLDER_PATH"
 cd bin
 rm -v $DEPL_FOLDER_PATH/bin/pro-sang-server
 git clone https://github.com/chiensitendo/pro-sang-server.git
