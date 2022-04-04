bc7d3ec598561faa7c483c28012b921097088860
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-04 22:35:06 +0700

    Change to use DEPL_FOLDER_PATH

--- .github/bin/deploy.sh
+++ .github/bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd bin
-rm -v $FOLDER_PATH/bin/pro-sang-server
+rm -v $DEPL_FOLDER_PATH/bin/pro-sang-server
 git clone https://github.com/chiensitendo/pro-sang-server.git
 cd pro-sang-server
 mvn clean package spring-boot:repackage
