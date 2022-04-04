9403429d764ea36790b1ed8802ee4f3d32f52051
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-04 23:05:38 +0700

    use real folder

--- .github/bin/deploy.sh
+++ .github/bin/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo "DEPL_FOLDER_PATH: $DEPL_FOLDER_PATH"
-echo "FOLDER_PATH: $FOLDER_PATH"
-cd bin
-rm -v $DEPL_FOLDER_PATH/bin/pro-sang-server
+rm -v ../deploy/repository/*
+cd repository
 git clone https://github.com/chiensitendo/pro-sang-server.git
 cd pro-sang-server
 mvn clean package spring-boot:repackage
+yes | cp -b target/pro-sang-server-0.0.1-SNAPSHOT.war ../deploy/bin/
