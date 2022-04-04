26e6731b5c5194af31daf46b8d79bcf7f26073ed
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-04 22:30:21 +0700

    Fix deploy script

--- .github/bin/deploy.sh
+++ .github/bin/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 cd bin
+rm -v $FOLDER_PATH/bin/pro-sang-server
 git clone https://github.com/chiensitendo/pro-sang-server.git
+cd pro-sang-server
+mvn clean package spring-boot:repackage
