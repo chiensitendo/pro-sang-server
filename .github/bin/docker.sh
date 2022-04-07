d43aa7ea9e956b92ec0d811807b314264a1428ff
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-07 20:20:50 +0700

    Fix docker.sh 4

--- .github/bin/docker.sh
+++ .github/bin/docker.sh
@@ -8,8 +8,13 @@ echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
-  "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-  && docker stop live-container \
-  && docker rm live-container \
-  && docker run --init -d --name live-container -p 80:8080 ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-  && docker system prune -af" # remove unused images to free up space
+  "echo this is a test!! "\
+  "echo "${DOCKER_HUB_USERNAME}""
+# ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
+#   "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+#   && docker stop live-container \
+#   && docker rm live-container \
+#   && docker run --init -d --name live-container -p 80:8080 ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+#   && docker system prune -af" # remove unused images to free up space
+
+# docker run --init -d --name live-container -p 8080:8080 --env-file=/deploy/.env chiensitendo/pro-sang-server:latest
