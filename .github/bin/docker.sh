d15dcc29d3a7cfb0fe17f1d7144a427d2f6a5131
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-07 21:51:22 +0700

    Fix docker.sh 8

--- .github/bin/docker.sh
+++ .github/bin/docker.sh
@@ -7,17 +7,17 @@ sudo chmod 600 ../private.key
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
-ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
-    "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest" \
-    "docker stop pro-sang-server-container" \
-    "docker rm pro-sang-server-container" \
-    "docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest" \
-    "docker system prune -af" \
-    "docker ps -a";
 # ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
-#   "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-#   && docker stop pro-sang-server-container \
-#   && docker rm pro-sang-server-container \
-#   && docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-#   && docker system prune -af" # remove unused images to free up space
+#     "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest" \
+#     "docker stop pro-sang-server-container" \
+#     "docker rm pro-sang-server-container" \
+#     "docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest" \
+#     "docker system prune -af" \
+#     "docker ps -a";
+ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
+  "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+  && docker stop pro-sang-server-container \
+  && docker rm pro-sang-server-container \
+  && docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+  && docker system prune -af" # remove unused images to free up space
 
