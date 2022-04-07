3e2fdc2e23a0bdc14de9262e86948ba8d9a048f2
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-07 17:36:29 +0700

    Fix docker.sh 2

--- .github/bin/docker.sh
+++ .github/bin/docker.sh
@@ -8,7 +8,7 @@ echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 DEPLOY_CMD = "echo ${DOCKER_HUB_ACCESS_TOKEN} | docker login --username ${DOCKER_HUB_USERNAME}"
-ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST} ${DEPLOY_CMD}"
+echo "${DEPLOY_CMD}" | ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST} "
 ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
   "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
   && docker stop live-container \
