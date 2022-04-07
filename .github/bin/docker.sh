6f001bd7a2ee27f38ed763d168d4f029e4e515ed
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-07 20:38:21 +0700

    Test deployment

--- .github/bin/docker.sh
+++ .github/bin/docker.sh
@@ -8,13 +8,9 @@ echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
-  "echo this is a test!! "\
-  "echo "${DOCKER_HUB_USERNAME}" > /deploy/testing.sh"
-# ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "root@${SSH_HOST}" \
-#   "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-#   && docker stop live-container \
-#   && docker rm live-container \
-#   && docker run --init -d --name live-container -p 80:8080 ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
-#   && docker system prune -af" # remove unused images to free up space
+  "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+  && docker stop pro-sang-server-container \
+  && docker rm pro-sang-server-container \
+  && docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest
+  && docker system prune -af" # remove unused images to free up space
 
-# docker run --init -d --name live-container -p 8080:8080 --env-file=/deploy/.env chiensitendo/pro-sang-server:latest
