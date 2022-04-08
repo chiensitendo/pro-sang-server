da6b68fb6a9887613935a019823f4de044bf4d86
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-08 10:52:33 +0700

    Completed Docker Deployment 1

--- .github/bin/deploy.sh
+++ .github/bin/deploy.sh
@@ -1,3 +1,16 @@
-rm -rf /deploy/repository/*
-cd /deploy/repository
-git clone https://github.com/chiensitendo/pro-sang-server.git
+# Stop script on first error
+set -e
+echo "Create SSH key"
+mkdir -p ~/.ssh/
+echo "$PRIVATE_KEY" > ../private.key
+sudo chmod 600 ../private.key
+echo "$KNOWN_HOSTS" > ~/.ssh/known_hosts
+
+echo "Deploying via remote SSH"
+ssh -i ../private.key -o UserKnownHostsFile=~/.ssh/known_hosts "${USER}@${HOST}" \
+  "docker pull ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+  && docker stop pro-sang-server-container \
+  && docker rm pro-sang-server-container \
+  && docker run --init -d --name pro-sang-server-container -p 8080:8080 --env-file=${ENVS} ${DOCKER_HUB_USERNAME}/pro-sang-server:latest \
+  && docker system prune -af" # remove unused images to free up space
+
--- backup/.github/bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-rm -rf /deploy/repository/*
-cd /deploy/repository
-git clone https://github.com/chiensitendo/pro-sang-server.git
