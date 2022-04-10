df3d04cadf1aae03c94b225a6b3d6b5168ceaf3b
chiensitendo <sang9c.nguyendu@gmail.com>
2022-04-10 23:17:46 +0700

    add i8n and error handling

--- src/main/java/com/sang/prosangserver/ProSangServerApplication.java
+++ src/main/java/com/sang/prosangserver/ProSangServerApplication.java
@@ -1,8 +1,14 @@
 package com.sang.prosangserver;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProSangServerApplication {
@@ -10,5 +16,19 @@ public class ProSangServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProSangServerApplication.class, args);
 	}
+	
+	@Bean  
+	public LocaleResolver localeResolver() {  
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);  
+		return localeResolver;  
+	}
+	
+	@Bean  
+	public ResourceBundleMessageSource messageSource() {  
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "error-messages");
+		return messageSource;  
+	}  
 
 }
--- src/main/java/com/sang/prosangserver/services/AccountService.java
+++ src/main/java/com/sang/prosangserver/services/AccountService.java
@@ -3,10 +3,13 @@ package com.sang.prosangserver.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.sang.prosangserver.dto.response.UserDetailResponse;
 import com.sang.prosangserver.entities.Account;
+import com.sang.prosangserver.enums.ErrorMessages;
 import com.sang.prosangserver.exceptions.UserNotFoundException;
 import com.sang.prosangserver.mappers.AccountUserDetailMapper;
 import com.sang.prosangserver.repositories.AccountRepository;
@@ -18,10 +21,15 @@ public class AccountService {
 	
 	private final AccountUserDetailMapper accountUserDetailMapper;
 	
+	private final MessageService messageService; 
 	
-	public AccountService(AccountRepository accountRepository, AccountUserDetailMapper accountUserDetailMapper) {
+	public AccountService(
+			AccountRepository accountRepository, 
+			AccountUserDetailMapper accountUserDetailMapper,
+			MessageService messageService) {
 		this.accountRepository = accountRepository;
 		this.accountUserDetailMapper = accountUserDetailMapper;
+		this.messageService = messageService;
 	}
 	
 	public List<UserDetailResponse> getAccountList() {
@@ -30,7 +38,8 @@ public class AccountService {
 	}
 	
 	public UserDetailResponse getAccountDetail(Long id) {
-		Account acc = accountRepository.getOneByIdAndIsDeletedIsFalse(id).orElseThrow(() -> new UserNotFoundException("this user is not found"));
+		Account acc = accountRepository.getOneByIdAndIsDeletedIsFalse(id)
+				.orElseThrow(() -> new UserNotFoundException(messageService.getMessage(ErrorMessages.USER_NOTFOUND)));
 		return accountUserDetailMapper.accountToUserDetail(acc);
 	}
 }
