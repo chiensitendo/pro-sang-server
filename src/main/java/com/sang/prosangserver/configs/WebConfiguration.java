b762ffc57e7f2c6a9b68eaf055a9613bbd776dd4
stran <stran@tripadvisor.com>
2022-10-13 09:32:11 +0700

    apply profile to webconfigs

--- src/main/java/com/sang/prosangserver/configs/WebConfiguration.java
+++ src/main/java/com/sang/prosangserver/configs/WebConfiguration.java
@@ -1,15 +1,18 @@
 package com.sang.prosangserver.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!prod")
 public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-			.allowedOrigins("http://localhost:3000", "https://pro-sang-app.vercel.app", "https://loinhac-lyric.vercel.app");
+			.allowedOrigins("http://localhost:3000");
 	}
 }
---
+++ src/main/java/com/sang/prosangserver/configs/WebProdConfiguration.java
@@ -0,0 +1,18 @@
+package com.sang.prosangserver.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("prod")
+public class WebProdConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedOrigins("https://pro-sang-app.vercel.app", "https://loinhac-lyric.vercel.app");
+    }
+}
