0dafdf7775c419607c171c5f981414e21c5ac302
chiensitendo <44012651+chiensitendo@users.noreply.github.com>
2022-10-13 09:07:32 +0700

    Delete target directory

--- target/test-classes/scripts/post-test-account.sql
+++
@@ -1,9 +0,0 @@
--- Delete User 1
-delete from account_auth where id = 1002;
-delete from account_detail where id = 1001;
-delete from account where id = 100;
-
--- Delete User 2
-delete from account_auth where id = 1012;
-delete from account_detail where id = 1011;
-delete from account where id = 101;
